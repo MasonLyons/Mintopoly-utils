507470731f89aa6a3009406fe9212ff8c468b6d9
Mason Lyons <masonlyons52@gmail.com>
2022-01-26 23:09:27 +0000

    slight refactor or get_rank

--- mintopoly.py
+++ mintopoly.py
@@ -9,9 +9,10 @@ def get_rank(round: int, address: str, bearer: str) -> int:
         "accept": "application/json, text/plain, */*",
         "authorization": f"Bearer {bearer}",
     }
+    url = f"https://api.mintopoly.io/game/round/{round}/player/{address}/rank"
     while rank == 0:
         response = requests.get(
-            f"https://api.mintopoly.io/game/round/{round}/player/{address}/rank",
+            url,
             headers=headers,
         )
         data = response.json()
